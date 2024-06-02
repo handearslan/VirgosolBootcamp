367b6928bde96393ffae5e0e63748979ca75744c
handearslan <hande_arslan1999@hotmail.com>
2024-06-02 21:43:12 +0300

    3st task done

---
+++ src/TaskThree/Question1.java
@@ -0,0 +1,13 @@
+package TaskThree;
+
+public class Question1 {
+    public static void main(String[] args) {
+        String message = "Bu bir mesajdır.";
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " - " + message);
+        }
+    }
+}
+
+/* 1. Bir mesajı 10 kez ekrana yazdıran bir Java programı yazın. */
---
+++ src/TaskThree/Question10.java
@@ -0,0 +1,21 @@
+package TaskThree;
+import java.util.Scanner;
+
+public class Question10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+
+        for (; number != 0; number /= 10) {
+            int digit = number % 10;
+            sum += digit;
+        }
+
+        System.out.println("Sayının basamakları toplamı: " + sum);
+        scanner.close();
+    }
+}
+/* 10. For döngüsü kullanarak bir sayının basamaklarını toplayan bir Java programı yazın. */
